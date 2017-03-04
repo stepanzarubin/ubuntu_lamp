2e61f01e18b1c417ca344941cabe09d4ac02cbbc
Stepan Zarubin <stepan.zarubin@gmail.com>
2017-03-04 18:12:35 +0200

    fix

--- ubuntu_lamp.sh
+++ ubuntu_lamp.sh
@@ -20,7 +20,7 @@ sudo apt-get update
 ### SOFTWARE ###
 
 #apache
-sudo apt-get -y install apache2 libapache2-mod-php
+sudo apt-get -y install apache2 libapache2-mod-php7.0 
 
 #php 7.0
 ./php/7.0.sh
